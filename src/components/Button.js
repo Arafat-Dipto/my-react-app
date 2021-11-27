9b71a029b91940d83a9b55a554a8aea54e9f5c01
arafat_hassan45 <diparafathassan@gmail.com>
2021-11-27 19:13:25 +0600

    initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import './Button.css'
+
+const STYLES = ['btn--primary', 'btn--outline']
+
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn-wide']
+
+const COLOR = ['primary', 'blue', 'red', 'green']
+
+export const Button = ({ children, type, onClick, buttonStyle, buttonSize, buttonColor }) => {
+    return (
+        // <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`}>{children}</button>
+    )
+}
+
